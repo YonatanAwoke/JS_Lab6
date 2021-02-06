4a08d202967f1fd815341307c3c39ab9f8b8f63e
Yonatan Awoke <it.yonatan.awoke@gmail.com>
2021-02-06 07:57:55 -0800

    Indexdb

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+// Define UI Variables 
+const taskInput = document.querySelector('#task'); //the task input text field
+const form = document.querySelector('#task-form'); //The form at the top
+const filter = document.querySelector('#filter'); //the task filter text field
+const taskList = document.querySelector('.collection'); //The UL
+const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+
+const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
+
+let DB;
+
+// Add Event Listener [on Load]
+document.addEventListener('DOMContentLoaded', () => {  
+    
+      //all code will reside here 
+      let TasksDB = indexedDB.open('tasks', 1);
+
+// if there's an error
+    TasksDB.onerror = function() {
+        console.log('There was an error');
+    }
+    // if everything is fine, assign the result to the instance
+    TasksDB.onsuccess = function() {
+  
+     console.log('Database Ready');
+
+    // save the result
+    DB = TasksDB.result;
+
+    // display the Task List 
+   // displayTaskList();
+
+    }
+
+    TasksDB.onupgradeneeded = function(e) {
+        // the event will be the database
+        let db = e.target.result;
+
+        // create an object store, 
+        // keypath is going to be the Indexes
+        let objectStore = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+
+        // createindex: 1) field name 2) keypath 3) options
+        objectStore.createIndex('taskname', 'taskname', { unique: false });
+
+        console.log('Database ready and fields created!');
+    }
+});
+
+
+
